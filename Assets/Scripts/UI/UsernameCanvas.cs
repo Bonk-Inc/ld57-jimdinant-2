e9f00fa523611ae33ee711f0f15c05a45e804c5d
Traxx186 <justinvanderkruit@icloud.com>
2025-04-07 22:15:34 +0200

    feat: add user registation

---
+++ Assets/Scripts/UI/UsernameCanvas.cs
@@ -0,0 +1,56 @@
+using System;
+using Bonk.BonkIncBackend;
+using Bonk.BonkIncBackend.Entities;
+using Bonk.BonkIncBackend.Settings;
+using Bonk.BonkIncBackend.UI;
+using Bonk.StandardLibrary.GameFlow.SceneLoading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UsernameCanvas : MonoBehaviour
+{
+    [SerializeField]
+    private RegisterUser register;
+    
+    [SerializeField]
+    private Button registerButton;
+    
+    [SerializeField]
+    private SceneLoader sceneLoader;
+    
+    [SerializeField]
+    private Canvas canvas;
+
+    private BabsDao _babsDao;
+    
+    private void Awake()
+    {
+        var settings = BonkIncBackendSettings.Load();
+        _babsDao = new BabsDao(settings);
+            
+        register.OnUserRegistered += HandleUserRegistered; 
+    }
+
+    private void OnEnable()
+    {
+        CheckUserRegistered();
+    }
+
+    public void SubmitUsername() =>
+        register.OnRegisterSubmit();
+
+    private void HandleUserRegistered(User _)
+    {
+        canvas.enabled = false;
+        sceneLoader.LoadScene("Play");
+    }
+
+    private void CheckUserRegistered()
+    {
+        if (string.IsNullOrWhiteSpace(PlayerPrefs.GetString(User.PlayerPrefField, string.Empty)))
+            return;
+        
+        Debug.Log("OnEnable");
+        HandleUserRegistered(new User());
+    }
+}
